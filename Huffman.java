7263850ac4e9c7dcada38bec97ef3762ff5d08b1
bigspiderk <144833020+bigspiderk@users.noreply.github.com>
2024-05-01 18:00:52 -0400

    Add files via upload

--- Huffman.java
+++ Huffman.java
@@ -1,27 +1,36 @@
-import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Huffman {
     public static void main(String[] args) {
-        // String s = "AAAAAABBBCCCCCCCD";
-        String s = null;
-        try {
-            FileInputStream in = new FileInputStream("test.webp");
-            s = new String(in.readAllBytes());
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        CompressedText ct = compress(s);
-        
-        if (decompress(ct).equals(s)) {
-            System.out.println("Decompression Worked!");
-            System.out.printf("Size of compressed text is %.2f%s of the original\n", ((double) ct.getLength()/s.length()) * 100, "%");
-        } else {
-            System.out.println("Decompression Failed");
+        if (args.length > 0) {
+            String file = null;
+            if (args.length >= 2) {
+                file = args[1];
+            } else {
+                System.err.println("Please Specify File");
+                System.exit(1);
+            }
+
+            if (args[0].equalsIgnoreCase("compress")) {
+                try {
+                    compress(file);
+                } catch (IOException e) {
+                    System.err.println("Compression failed");
+                }
+            }
+
+            if (args[0].equalsIgnoreCase("decompress")) {
+                try {
+                    decompress(file);
+                } catch (IOException e) {
+                    System.err.println("Decompression failed");
+                }
+            }
         }
     }
 
@@ -51,9 +60,83 @@ public class Huffman {
         return new Node[]{nodes.get(n), nodes.get(n+1)};
     }
 
-    public static CompressedText compress(String s) {
-        HashMap<Character, Integer> frequencies = new HashMap<Character, Integer>();
-        for (char c : s.toCharArray()) {
+    public static void writeTable(FileOutputStream out, HashMap<Integer, String> table) {
+        try {
+            out.write(table.size());
+            StringBuffer bitBuffer = new StringBuffer();
+            for (int c : table.keySet()) {
+                bitBuffer.append(intToBin(c));
+                bitBuffer.append(intToBin(table.get(c).length()).substring(0,3));
+                bitBuffer.append(table.get(c));
+                while (bitBuffer.length() >= 8) {
+                    out.write(binToInt(bitBuffer.substring(0,8)));
+                    bitBuffer.delete(0,8);
+                }
+            }
+            if (bitBuffer.length() > 0) {
+                while (bitBuffer.length() < 8) {
+                    bitBuffer.append(0);
+                }
+                out.write(binToInt(bitBuffer.toString()));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static HashMap<String, Integer> parseTable(FileInputStream in) {
+        HashMap<String, Integer> table = new HashMap<String, Integer>();
+        try {
+            int tableSize = in.read();
+            StringBuffer bitBuffer = new StringBuffer();
+            for (int i = 0; i < tableSize; i++) {
+                if (bitBuffer.length() < 8) {
+                    bitBuffer.append(intToBin(in.read()));
+                }
+                char c = (char) binToInt(bitBuffer.substring(0,8));
+                bitBuffer.delete(0, 8);
+                if (bitBuffer.length() < 3) {
+                    bitBuffer.append(intToBin(in.read()));
+                }
+                int codeSize = binToInt(bitBuffer.substring(0,3));
+                bitBuffer.delete(0, 3);
+                if (bitBuffer.length() < codeSize) {
+                    bitBuffer.append(intToBin(in.read()));
+                }
+                table.put(bitBuffer.substring(0, codeSize), (int) c);
+                bitBuffer.delete(0, codeSize);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return table;
+    }
+
+    public static ArrayList<String> generateCodes(int len) {
+        ArrayList<String> codes = new ArrayList<String>();
+
+        for (int i = 0; i < Math.pow(2, len); i++) {
+            codes.add(intToBin(i).substring(0, len));
+        }
+
+        return codes;
+    }
+
+
+    public static void compress(String file) throws IOException{
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            System.err.println("Cannot Find File");
+            System.exit(1);
+        }
+
+        HashMap<Integer, Integer> frequencies = new HashMap<Integer, Integer>();
+        int c;
+        while ((c = in.read()) != -1) {
             if (frequencies.containsKey(c)) {
                 frequencies.put(c, frequencies.get(c) + 1);
             } else {
@@ -61,8 +144,10 @@ public class Huffman {
             }
         }
 
+        in.close();
+
         ArrayList<Node> nodes = new ArrayList<>();
-        for (char i : frequencies.keySet()) {
+        for (int i : frequencies.keySet()) {
             nodes.add(new Node(frequencies.get(i), (char) i));
         }
 
@@ -73,85 +158,160 @@ public class Huffman {
         while (nodes.size() > 1) {
             smallestPair = getSmallestPair(nodes);
             Node parent = new Node(smallestPair[0].getValue() + smallestPair[1].getValue());
-            nodes.set(nodes.indexOf(smallestPair[0]), parent);
+            nodes.add(nodes.indexOf(smallestPair[0]), parent);
             for (int i = 0; i < smallestPair.length; i++) {
                 smallestPair[i].setParent(parent, i);
                 nodes.remove(smallestPair[i]);
             }
         }
 
-        HashMap<Character, String> table = new HashMap<Character, String>();
+        HashMap<Integer, String> table = new HashMap<Integer, String>();
         String currentPath;
         for (Node leaf : leaves) {
             if ((currentPath = leaf.getPath()) != null) {
-                table.put(leaf.getChar(), currentPath);
+                table.put((int) leaf.getChar(), currentPath);
             }
         }
 
-        ByteArrayOutputStream compressedBytes = new ByteArrayOutputStream();
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (table.containsKey(c)) {
-                sb.append(table.get(c));
+        if (table.size() < 3) {
+            table = new HashMap<Integer, String>();
+            sort(leaves);
+    
+            ArrayList<String> codes = new ArrayList<String>();
+            for (int i = 1; i < 7; i++) {
+                if (generateCodes(i).size() <= leaves.size()) {
+                    codes = generateCodes(i);
+                }
             }
-            if (sb.length() >= 8) {
-                compressedBytes.write(binToInt(sb.substring(0, 8)));
+            
+            for (int i = 0; i < codes.size(); i++) {
+                table.put((int) leaves.get(leaves.size()-i-1).getChar(), codes.get(codes.size()-i-1));
+            }
+        }
+
+        FileOutputStream out = new FileOutputStream(file + ".comp");
+        writeTable(out, table);
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            in = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            System.err.println("Cannot Find File");
+            System.exit(1);
+        }
+
+        int b;
+        while ((b = in.read()) != -1) {
+            if (table.containsKey(b)) {
+                sb.append(1);
+                sb.append(table.get(b));
+            } else {
+                sb.append(0);
+                sb.append(intToBin(b));
+            }
+
+            while (sb.length() >= 8) {
+                out.write(binToInt(sb.substring(0, 8)));
                 sb.delete(0, 8);
             }
         }
 
-        int ignoredBits = 8-sb.length();
-        for (int i = 0; i < ignoredBits; i++) {
-            sb.append("0");
-        }
-
-        compressedBytes.write((byte) binToInt(sb.toString()));
-
-        return new CompressedText(table, compressedBytes.toByteArray(), ignoredBits);
-    }
-
-    public static String decompress(CompressedText ct) {
-        StringBuilder bitBuffer = new StringBuilder();
-        StringBuilder decompressedText = new StringBuilder();
-        byte[] compressedText = ct.getBytes();
-        
-        for (int i = 0; i < compressedText.length; i++) {
-            int endIndex = 8-(ct.getBitsIgnored() * (i == compressedText.length-1 ? 1 : 0));
-            bitBuffer.append(intToBin((int) compressedText[i] & 0xff).substring(0, endIndex));
-            for (int j = 0; j < bitBuffer.length()+1; j++) {
-                if (ct.getTable().containsKey(bitBuffer.substring(0, j))) {
-                    decompressedText.append(ct.getTable().get(bitBuffer.substring(0, j)));
-                    bitBuffer.delete(0, j);
-                    j = 0;
-                }
+        if (sb.length() > 0) {
+            while (sb.length() < 8) {
+                sb.append("0");
             }
         }
 
-        return decompressedText.toString();
+        out.write((byte) binToInt(sb.toString()));
+        in.close();
+        out.close();
+    }
+
+    public static void decompress(String file) throws IOException {
+        StringBuilder bitBuffer = new StringBuilder();
+        String outPath = String.format("decomp-%s", file.substring(0, file.lastIndexOf(".comp")));
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        try {
+            in = new FileInputStream(file);
+            out = new FileOutputStream(outPath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        HashMap<String, Integer> table = parseTable(in);
+
+        bitBuffer.append(intToBin(in.read()));
+
+        while (bitBuffer.length() > 0) {
+            boolean inTable = bitBuffer.charAt(0) == '1';
+            bitBuffer.deleteCharAt(0);
+            if (inTable) {
+                StringBuilder currentCode = new StringBuilder();
+                while (!table.containsKey(currentCode.toString())) {
+                    try{
+                        if (bitBuffer.length() < 1) {
+                            bitBuffer.append(intToBin(in.read()));
+                        }
+                        currentCode.append(bitBuffer.charAt(0));
+                        bitBuffer.deleteCharAt(0);
+                    } catch (StringIndexOutOfBoundsException e2) {
+                        currentCode = null;
+                        break;
+                    }
+                }
+
+                if (currentCode != null) {
+                    out.write(table.get(currentCode.toString()));
+                    if (bitBuffer.length() < 1) {
+                        bitBuffer.append(intToBin(in.read()));
+                    }
+                }
+
+            } else {
+                try{
+                    if (bitBuffer.length() < 8) {
+                        bitBuffer.append(intToBin(in.read()));
+                    }
+                    out.write(binToInt(bitBuffer.substring(0,8)));
+                    bitBuffer.delete(0, 8);
+                } catch (StringIndexOutOfBoundsException ignored) {}
+            }
+            
+            if (bitBuffer.length() < 8) {
+                bitBuffer.append(intToBin(in.read()));
+            }
+        }
+        in.close();
+        out.close();
     }
 
     public static int binToInt(String bin) {
         int total = 0;
-        char[] bits = bin.toCharArray();
-        for (int i = bits.length-1; i >= 0; i--) {
-            total += Math.pow(2, i) * ((int)bits[bin.length()-i-1] % 48);
+        for (int i = 0; i < bin.length(); i++) {
+            total += Math.pow(2, i) * (bin.charAt(i) % '0');
         }
         return total;
     }
 
     public static String intToBin(int num) {
-        int remainder = num;
-        StringBuilder bin = new StringBuilder();
-        for (int i = 7; i >= 0; i--) {
-            if (Math.pow(2, i) <= remainder) {
-                remainder -= Math.pow(2, i);
-                bin.append(1);
-            } else {
-                bin.append(0);
-            }
-        }
+        StringBuilder binary = new StringBuilder();
 
-        return bin.toString();
+        if (num == 0) {
+            binary.append(0);
+        }
+    
+        while (num > 0) {
+            int remainder = num % 2;
+            binary.append(remainder);
+            num /= 2;
+        }
+    
+        while (binary.length() % 8 != 0) {
+            binary.append('0');
+        }
+    
+        return binary.toString();
     }
 
 }
@@ -197,43 +357,12 @@ class Node {
             return path.toString();
         }
 
+        if (path.length() == 7) {
+            return null;
+        }
+
         path.insert(0, position);
         return parent.getPath(path);
     }
 
-}
-
-class CompressedText {
-    private HashMap<String, Character> table;
-    private byte[] text;
-    private int bitsIgnored;
-
-    public CompressedText(HashMap<Character, String> table, byte[] text, int bitsIgnored) {
-        this.text = text;
-        this.bitsIgnored = bitsIgnored;
-        this.table = new HashMap<String, Character>();
-        for (char c : table.keySet()) {
-            this.table.put(table.get(c), c);
-        }
-    }
-
-    public int getBitsIgnored() {
-        return bitsIgnored;
-    }
-
-    public byte[] getBytes() {
-        return text;
-    }
-
-    public HashMap<String, Character> getTable() {
-        return table;
-    }
-
-    public int getLength() {
-        return text.length;
-    }
-
-    public String toString() {
-        return new String(text);
-    }
 }
